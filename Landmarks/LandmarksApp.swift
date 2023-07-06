8fbc3a53ad7c4bfea42415df6da8bd4eb109c10c
Aayush Agarwal <ayush2991@gmail.com>
2023-07-05 23:37:10 -0400

    Add hello world template. Update gitignore file.wq

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Aayush Agarwal on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
